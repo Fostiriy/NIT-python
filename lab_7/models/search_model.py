f41559e1b08ed9aea10910063934201ee8e8243b
Fostiriy <burakov.aa@students.dvfu.ru>
2022-11-16 20:37:50 +1000

    Лаб_7: Исправить запрос на поиск

--- lab_7/models/search_model.py
+++ lab_7/models/search_model.py
@@ -31,21 +31,30 @@ def get_author(conn):
 
 def get_book(conn, genres, authors, publishers):
     return pd.read_sql(f'''
-SELECT b.book_id,
-       title                              Название,
-       group_concat(DISTINCT author_name) Авторы,
-       genre_name                         Жанр,
-       publisher_name                     Издательство,
-       year_publication                   Год_издания,
-       available_numbers                  Количество
-FROM book b
-         INNER JOIN book_author ba on b.book_id = ba.book_id
+WITH books_query AS (SELECT b.book_id,
+                            title             Название,
+                            genre_name        Жанр,
+                            publisher_name    Издательство,
+                            year_publication  Год_издания,
+                            available_numbers Количество
+                     FROM book b
+                              INNER JOIN book_author ba on b.book_id = ba.book_id
+                              INNER JOIN author a on a.author_id = ba.author_id
+                         AND (author_name IN ({str(authors).strip('[]')}) OR {not authors})
+                              INNER JOIN genre g on g.genre_id = b.genre_id
+                         AND (genre_name IN ({str(genres).strip('[]')}) OR {not genres})
+                              INNER JOIN publisher p on p.publisher_id = b.publisher_id
+                         AND (publisher_name IN ({str(publishers).strip('[]')}) OR {not publishers})
+                     GROUP BY b.book_id)
+SELECT bq.book_id,
+       Название,
+       Жанр,
+       Издательство,
+       Год_издания,
+       Количество,
+       group_concat(author_name) Авторы
+FROM books_query bq
+         INNER JOIN book_author ba on bq.book_id = ba.book_id
          INNER JOIN author a on a.author_id = ba.author_id
-         INNER JOIN genre g on g.genre_id = b.genre_id
-    AND (genre_name IN ({str(genres).strip('[]')}) OR {not genres})
-         INNER JOIN publisher p on p.publisher_id = b.publisher_id
-    AND (publisher_name IN ({str(publishers).strip('[]')}) OR {not publishers})
-GROUP BY b.book_id
-HAVING (author_name IN ({str(authors).strip('[]')})
-    OR {not authors})
-    ''', conn)
+GROUP BY bq.book_id
+''', conn)
